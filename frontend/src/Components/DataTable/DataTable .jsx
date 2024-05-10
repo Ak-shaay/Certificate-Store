2c5f15692a6a788338943d9c47e1ed453ffa8975
Bk-Rudra <bhuppi.rudra@gmail.com>
2024-05-10 17:24:59 +0530

    Filters Alignment

--- frontend/src/Components/DataTable/DataTable .jsx
+++ frontend/src/Components/DataTable/DataTable .jsx
@@ -7,95 +7,93 @@ import download from "../../Images/download.png";
 import verify from "../../Images/check-mark.png";
 import exclamation from "../../Images/exclamation.png";
 
-
 const DataTable = () => {
-
   function getIndianRegion(state) {
     const regions = {
-        'Andhra Pradesh': 'South',
-        'Arunachal Pradesh': 'Northeast',
-        'Assam': 'Northeast',
-        'Bihar': 'East',
-        'Chhattisgarh': 'Central',
-        'Goa': 'West',
-        'Gujarat': 'West',
-        'Haryana': 'North',
-        'Himachal Pradesh': 'North',
-        'Jharkhand': 'East',
-        'Karnataka': 'South',
-        'Kerala': 'South',
-        'Madhya Pradesh': 'Central',
-        'Maharashtra': 'West',
-        'Manipur': 'Northeast',
-        'Meghalaya': 'Northeast',
-        'Mizoram': 'Northeast',
-        'Nagaland': 'Northeast',
-        'Odisha': 'East',
-        'Punjab': 'North',
-        'Rajasthan': 'West',
-        'Sikkim': 'Northeast',
-        'Tamil Nadu': 'South',
-        'Telangana': 'South',
-        'Tripura': 'Northeast',
-        'Uttar Pradesh': 'North',
-        'Uttarakhand': 'North',
-        'West Bengal': 'East',
-        'Andaman and Nicobar Islands': 'South',
-        'Chandigarh': 'North',
-        'Dadra and Nagar Haveli': 'West',
-        'Daman and Diu': 'West',
-        'Delhi': 'North',
-        'Lakshadweep': 'South',
-        'Puducherry': 'South',
-        'AP': 'South',
-        'AR': 'Northeast',
-        'AS': 'Northeast',
-        'BR': 'East',
-        'CG': 'Central',
-        'GA': 'West',
-        'GJ': 'West',
-        'HR': 'North',
-        'HP': 'North',
-        'JH': 'East',
-        'KA': 'South',
-        'KL': 'South',
-        'MP': 'Central',
-        'MH': 'West',
-        'MN': 'Northeast',
-        'ML': 'Northeast',
-        'MZ': 'Northeast',
-        'NL': 'Northeast',
-        'OD': 'East',
-        'PB': 'North',
-        'RJ': 'West',
-        'SK': 'Northeast',
-        'TN': 'South',
-        'TS': 'South',
-        'TR': 'Northeast',
-        'UP': 'North',
-        'UK': 'North',
-        'WB': 'East',
-        'AN': 'South',
-        'CH': 'North',
-        'DN': 'West',
-        'DD': 'West',
-        'DL': 'North',
-        'LD': 'South',
-        'PY': 'South'
+      "Andhra Pradesh": "South",
+      "Arunachal Pradesh": "Northeast",
+      Assam: "Northeast",
+      Bihar: "East",
+      Chhattisgarh: "Central",
+      Goa: "West",
+      Gujarat: "West",
+      Haryana: "North",
+      "Himachal Pradesh": "North",
+      Jharkhand: "East",
+      Karnataka: "South",
+      Kerala: "South",
+      "Madhya Pradesh": "Central",
+      Maharashtra: "West",
+      Manipur: "Northeast",
+      Meghalaya: "Northeast",
+      Mizoram: "Northeast",
+      Nagaland: "Northeast",
+      Odisha: "East",
+      Punjab: "North",
+      Rajasthan: "West",
+      Sikkim: "Northeast",
+      "Tamil Nadu": "South",
+      Telangana: "South",
+      Tripura: "Northeast",
+      "Uttar Pradesh": "North",
+      Uttarakhand: "North",
+      "West Bengal": "East",
+      "Andaman and Nicobar Islands": "South",
+      Chandigarh: "North",
+      "Dadra and Nagar Haveli": "West",
+      "Daman and Diu": "West",
+      Delhi: "North",
+      Lakshadweep: "South",
+      Puducherry: "South",
+      AP: "South",
+      AR: "Northeast",
+      AS: "Northeast",
+      BR: "East",
+      CG: "Central",
+      GA: "West",
+      GJ: "West",
+      HR: "North",
+      HP: "North",
+      JH: "East",
+      KA: "South",
+      KL: "South",
+      MP: "Central",
+      MH: "West",
+      MN: "Northeast",
+      ML: "Northeast",
+      MZ: "Northeast",
+      NL: "Northeast",
+      OD: "East",
+      PB: "North",
+      RJ: "West",
+      SK: "Northeast",
+      TN: "South",
+      TS: "South",
+      TR: "Northeast",
+      UP: "North",
+      UK: "North",
+      WB: "East",
+      AN: "South",
+      CH: "North",
+      DN: "West",
+      DD: "West",
+      DL: "North",
+      LD: "South",
+      PY: "South",
     };
-    
-    return regions[state] || 'Unknown';
-}
 
-const handleFilters = (e) => {
-  const filtersElement = document.getElementById('filter');
-      filtersElement.style.display = 'block';
-}
+    return regions[state] || "Unknown";
+  }
 
-const handleFilterClose = (e) => {
-  const filtersElement = document.getElementById('filter');
-      filtersElement.style.display = 'none';
-}
+  const handleFilters = (e) => {
+    const filtersElement = document.getElementById("filter");
+    filtersElement.style.display = "block";
+  };
+
+  const handleFilterClose = (e) => {
+    const filtersElement = document.getElementById("filter");
+    filtersElement.style.display = "none";
+  };
   const wrapperRef = useRef(null);
 
   const grid = new Grid({
@@ -116,31 +114,63 @@ const handleFilterClose = (e) => {
       "Validity",
       "Status",
       {
-        name: 'Actions',
+        name: "Actions",
         formatter: (cell, row) => {
-            return h('div', {className:'action-row'}, [
-                h('div', {
-                    className: '',
-                    onClick: () => alert(`view "${row.cells[0].data}" "${row.cells[1].data}"`)
-                }, [
-                    h('img', { src: exclamation, alt: 'View' ,className: 'action-img',title:'View'})
-                ]),
-                h('div', {
-                    className: '',
-                    onClick: () => alert(`download "${row.cells[0].data}" "${row.cells[1].data}"`)
-                }, [
-                    h('img', { src: download, alt: 'Download',className: 'action-img', title:'Download' })
-                ]),
-                h('div', {
-                    className: '',
-                    onClick: () => alert(`Verify "${row.cells[0].data}" "${row.cells[1].data}"`)
-                }, [
-                    h('img', { src: verify, alt: 'Verifying' ,className: 'action-img', title:'Verify'})
-                ])
-            ]);
-        }
-    },
-],
+          return h("div", { className: "action-row" }, [
+            h(
+              "div",
+              {
+                className: "",
+                onClick: () =>
+                  alert(`view "${row.cells[0].data}" "${row.cells[1].data}"`),
+              },
+              [
+                h("img", {
+                  src: exclamation,
+                  alt: "View",
+                  className: "action-img",
+                  title: "View",
+                }),
+              ]
+            ),
+            h(
+              "div",
+              {
+                className: "",
+                onClick: () =>
+                  alert(
+                    `download "${row.cells[0].data}" "${row.cells[1].data}"`
+                  ),
+              },
+              [
+                h("img", {
+                  src: download,
+                  alt: "Download",
+                  className: "action-img",
+                  title: "Download",
+                }),
+              ]
+            ),
+            h(
+              "div",
+              {
+                className: "",
+                onClick: () =>
+                  alert(`Verify "${row.cells[0].data}" "${row.cells[1].data}"`),
+              },
+              [
+                h("img", {
+                  src: verify,
+                  alt: "Verifying",
+                  className: "action-img",
+                  title: "Verify",
+                }),
+              ]
+            ),
+          ]);
+        },
+      },
+    ],
     server: {
       url: "http://localhost:8080/data",
       method: "POST",
@@ -151,18 +181,16 @@ const handleFilterClose = (e) => {
           ca.issuer_name,
           ca.issue_date,
           ca.subject_state,
-          getIndianRegion( ca.subject_state),
+          getIndianRegion(ca.subject_state),
           "India",
           ca.expiry_date,
           "Status",
-         null,
-        ]
-        ),
+          null,
+        ]),
     },
   });
   useEffect(() => {
     grid.render(wrapperRef.current);
-    
   }, []);
 
   const options = [
@@ -177,22 +205,35 @@ const handleFilterClose = (e) => {
   return (
     <div className="MainTable">
       <div className="filterWindow" id="filter">
-        <span className="close" onClick={handleFilterClose}>X</span>
+        <span className="close" onClick={handleFilterClose}>
+          X
+        </span>
         <div className="multi-select-row">
-      <MultiSelect
-          options={options}
-          placeholder="Select Issuer"
-          onChange={handleMultiSelectChange}
-        />
-        <MultiSelect options={options} placeholder="Select State" />
-        <MultiSelect options={options} placeholder="Select Region" />
-        <input type="date" class="datepicker"/>
-        <input type="date" class="datepicker"/>
-       <button className="apply-btn">Apply</button>
-       </div>
+          <MultiSelect
+            options={options}
+            placeholder="Select Issuer"
+            onChange={handleMultiSelectChange}
+          />
+          <MultiSelect options={options} placeholder="Select State" />
+          <MultiSelect options={options} placeholder="Select Region" />
+          </div>
+          <div className="col">
+          <div className="row date_picker">
+            <label className="dateLable">Start Date</label>
+            <input type="date" className="datepicker" />
+            <label className="dateLable">End Date</label>
+            <input type="date" className="datepicker" />
+          </div>
+          <br/>
+          <div className="row date_picker">
+          <button className="apply-btn">Apply</button>
+        </div>
+        </div>
       </div>
-      <button class="filter-btn" onClick={handleFilters}>Filters</button>
-      
+      <button class="filter-btn" onClick={handleFilters}>
+        Filters
+      </button>
+
       <div className="table-container" ref={wrapperRef} />
     </div>
   );
