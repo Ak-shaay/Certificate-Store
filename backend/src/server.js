3fb8c00b1ae94b44afc91cdafc826d2f8a725928
Bk-Rudra <bhuppi.rudra@gmail.com>
2024-06-14 18:05:30 +0530

    ipconfiguration

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ const fileUpload = require("express-fileupload");
 app.use(fileUpload());
 
 const corsOptions = {
-  origin: 'http://192.168.1.110:3000',
+  origin: 'http://192.168.10.248:3000',
   credentials: true,
 };
 app.use(cors(corsOptions));
