fec7e6b54af8890b6c34583b68ee4ef01e7e400e
Ak-shaay <akshay1999achu@gmail.com>
2025-06-12 14:52:05 +0530

    profile image adjustments

--- frontend/src/Components/RightSideAccount/RightSideAccount.jsx
+++ frontend/src/Components/RightSideAccount/RightSideAccount.jsx
@@ -3,6 +3,7 @@ import "./RightSideAccount.css";
 import api from "../../Pages/axiosInstance";
 
 import { domain } from "../../Context/config";
+
 const RightSideAccount = () => {
   const [username, setUsername] = useState(null);
   const [authNo, setAuthNo] = useState("");
@@ -11,8 +12,11 @@ const RightSideAccount = () => {
 
   const [imageError, setImageError] = useState(false);
 
-  const imgURL = "http://" + domain + ":8080/images/" + authNo + ".png" || "";
-  const defaultImage = "http://" + domain + ":8080/images/null.png";   
+  const imgURL = authNo
+    ? "http://" + domain + ":8080/images/" + authNo + ".png"
+    : "";
+  const defaultImage = "http://" + domain + ":8080/images/null.png";
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -23,7 +27,7 @@ const RightSideAccount = () => {
         const response = await api.axiosInstance.get("/profile");
         if (response.status !== 200) {
           throw new Error("Network response was not ok");
-        } else {          
+        } else {
           const base64Url = accessToken.split(".")[1];
           const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
           const decodedToken = JSON.parse(atob(base64));
@@ -40,26 +44,35 @@ const RightSideAccount = () => {
   }, []);
 
   const handleImageError = () => {
-    setImageError(true); 
+    setImageError(true);
   };
+
   return (
-      <div className="RightMain">
-        <div className="ProfileContainer">
-          <div className="Profile">
-            <img  src={imageError ? defaultImage : imgURL}
+    <div className="RightMain">
+      <div className="ProfileContainer">
+        <div className="Profile">
+          <img
+            src={imageError ? defaultImage : imgURL}
             alt="profile"
-            onError={handleImageError}  />
-            <div className="ProfileData">
-              <h3 className="ProfileName">{username}</h3>
-              <strong>Login Details</strong>
-              <div className="ProfileStatus">
-                <p className="status"><b>IP Address : </b>{ipAddress != null ? ipAddress : ""}</p>
-                <p className="status"><b>Time : </b>{lastLogin != null ? lastLogin : ""}</p>
-              </div>
+            onError={handleImageError}
+          />
+          <div className="ProfileData">
+            <h3 className="ProfileName">{username}</h3>
+            <strong>Login Details</strong>
+            <div className="ProfileStatus">
+              <p className="status">
+                <b>IP Address : </b>
+                {ipAddress != null ? ipAddress : ""}
+              </p>
+              <p className="status">
+                <b>Time : </b>
+                {lastLogin != null ? lastLogin : ""}
+              </p>
             </div>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
