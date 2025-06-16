0ae2eb8c971cbf1dc7c4af2dbc5a67640674a390
Ak-shaay <akshay1999achu@gmail.com>
2025-06-16 18:14:05 +0530

    pm2 config update

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,12 +1,15 @@
+const path = require('path');
+
 module.exports = {
-  apps : [{
+  apps: [{
+    name: 'my-app',
     script: './src/server.js',
-    out_file: "./out.log",
-    error_file: "./error.log",
-    watch: '.',
+    out_file: './out.log',
+    error_file: './error.log',
+    watch: [path.resolve(__dirname, 'src')],
     ignore_watch: [
-                "node_modules",
-                "tokens.json",
-            ],
+      path.resolve(__dirname, 'tokens.json'),
+      path.resolve(__dirname, 'node_modules'),
+    ],
   }],
-};
+}
